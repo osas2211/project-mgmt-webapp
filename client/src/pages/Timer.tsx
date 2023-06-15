d7cf8373c44ade24352ad3ac5e3eaf3b2db497e8
osaretin frank <osaretin.frank10@gmail.com>
2023-06-15 02:55:18 +0100

    Ready for submission

--- client/src/pages/Timer.tsx
+++ client/src/pages/Timer.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { TimerPage } from "./TimerPage"
 import { ConfigureTimer } from "../components/ConfigureTimer"
+import { FormatQuoteTwoTone } from "@mui/icons-material"
 
 export const Timer = () => {
   const [isConfigure, setIsConfigure] = useState(false)
@@ -19,6 +20,24 @@ export const Timer = () => {
   }, [isConfigure])
   return (
     <div className="page app__section">
+      <div
+        style={{
+          display: "inline-flex",
+          fontFamily: "cursive",
+          background: "rgba(197, 209, 235, 0.3)",
+          padding: "0.7rem",
+          borderRadius: "8px",
+          border: "1px solid rgba(197, 209, 235, 0.9)",
+        }}
+      >
+        <FormatQuoteTwoTone />
+        <div style={{ marginLeft: "0.3rem" }}>
+          <p>Focus on being productive instead of busy.</p>
+          <small>
+            <i>~ Tim Ferriss, American podcaster, author and entrepreneur</i>
+          </small>
+        </div>
+      </div>
       <TimerPage
         updateConfigure={updateConfigure}
         pomodoro={pomodoro}
